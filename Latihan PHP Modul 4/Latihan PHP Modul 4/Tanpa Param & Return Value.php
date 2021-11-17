7f02d05ab4b3a9aadedb133b52a06925428fbebb
mutiara-nur <91928175+mutiara-nur@users.noreply.github.com>
2021-11-17 16:03:56 +0700

    Add files via upload

---
+++ Latihan PHP Modul 4/Latihan PHP Modul 4/Tanpa Param & Return Value.php
@@ -0,0 +1,18 @@
+<?php
+function luas_lingkaran ($jari) {
+    return 3.14 * $jari * $jari;
+}
+$r = 10;
+echo "Luas lingkaran dengan jari-jari $r = ";
+echo luas_lingkaran($r);
+
+echo "<hr>";
+
+function head() {
+    echo'
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="style/font-awesome.css"> ';
+}
+
+echo luas_lingkaran($r);
+?>
